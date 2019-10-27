0ec3eb44716e5b6e2678d8bd579f0f5975cb970b
Vicente Romero <vteromero@gmail.com>
2019-10-27 17:12:55 +0000

    add MIT license

--- encode.c
+++ encode.c
@@ -1,3 +1,8 @@
+/**
+  Copyright (c) 2019 Vicente Romero Calero. All rights reserved.
+  Licensed under the MIT License.
+  See LICENSE file in the project root for full license information.
+ */
 #include "vtenc.h"
 
 #include "internals.h"
