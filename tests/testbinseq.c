8787cc5c5a07142b660b816cd94a39d1001bf9d1
Vicente Romero <vteromero@gmail.com>
2020-04-12 07:08:07 +0100

    refactor tests/testbinseq program to use functions on encdec.h

--- tests/testbinseq.c
+++ tests/testbinseq.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "encdec.h"
 #include "../mem.h"
 #include "../vtenc.h"
 
@@ -199,598 +200,107 @@ static int read_arr64(FILE *f, uint64_t *arr, size_t arr_len)
 //   printf("\n");
 // }
 
-static int test_encdec_list8(uint8_t *list, size_t list_len)
+static int test_seq8(FILE *f, VtencEncoder *encoder, size_t seqsize)
 {
-  size_t enc_out_cap, enc_out_len, dec_out_len;
-  uint8_t *enc_out=NULL, *dec_out=NULL;
-  VtencErrorCode enc_ret_code, dec_ret_code;
-  int ret_code = 1;
+  uint8_t *seq = (uint8_t *) malloc(seqsize * sizeof(uint8_t));
 
-  enc_out_cap = vtenc_list_max_encoded_size_u8(list_len);
-  enc_out = (uint8_t *) malloc(enc_out_cap * sizeof(uint8_t));
-  if (enc_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  enc_ret_code = vtenc_list_encode_u8(list, list_len, enc_out, enc_out_cap, &enc_out_len);
-  if (enc_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "encode error code: %d\n", enc_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_out_len = vtenc_list_decoded_size_u8(enc_out, enc_out_len);
-  dec_out = (uint8_t *) malloc(dec_out_len * sizeof(uint8_t));
-  if (dec_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_ret_code = vtenc_list_decode_u8(enc_out, enc_out_len, dec_out, dec_out_len);
-  if (dec_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "decode error code: %d\n", dec_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  if ((dec_out_len != list_len) || (memcmp(list, dec_out, list_len) != 0)) {
-    fprintf(stderr, "decoded output different from original input\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-free_and_exit:
-  if (enc_out != NULL) free(enc_out);
-  if (dec_out != NULL) free(dec_out);
-
-  return ret_code;
-}
-
-static int test_encdec_list16(uint16_t *list, size_t list_len)
-{
-  size_t enc_out_cap, enc_out_len, dec_out_len;
-  uint8_t *enc_out = NULL;
-  uint16_t *dec_out = NULL;
-  VtencErrorCode enc_ret_code, dec_ret_code;
-  int ret_code = 1;
-
-  enc_out_cap = vtenc_list_max_encoded_size_u16(list_len);
-  enc_out = (uint8_t *) malloc(enc_out_cap * sizeof(uint8_t));
-  if (enc_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  enc_ret_code = vtenc_list_encode_u16(list, list_len, enc_out, enc_out_cap, &enc_out_len);
-  if (enc_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "encode error code: %d\n", enc_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_out_len = vtenc_list_decoded_size_u16(enc_out, enc_out_len);
-  dec_out = (uint16_t *) malloc(dec_out_len * sizeof(uint16_t));
-  if (dec_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_ret_code = vtenc_list_decode_u16(enc_out, enc_out_len, dec_out, dec_out_len);
-  if (dec_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "decode error code: %d\n", dec_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  if ((dec_out_len != list_len) || (memcmp(list, dec_out, list_len * sizeof(uint16_t)) != 0)) {
-    fprintf(stderr, "decoded output different from original input\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-free_and_exit:
-  if (enc_out != NULL) free(enc_out);
-  if (dec_out != NULL) free(dec_out);
-
-  return ret_code;
-}
-
-static int test_encdec_list32(uint32_t *list, size_t list_len)
-{
-  size_t enc_out_cap, enc_out_len, dec_out_len;
-  uint8_t *enc_out = NULL;
-  uint32_t *dec_out = NULL;
-  VtencErrorCode enc_ret_code, dec_ret_code;
-  int ret_code = 1;
-
-  enc_out_cap = vtenc_list_max_encoded_size_u32(list_len);
-  enc_out = (uint8_t *) malloc(enc_out_cap * sizeof(uint8_t));
-  if (enc_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  enc_ret_code = vtenc_list_encode_u32(list, list_len, enc_out, enc_out_cap, &enc_out_len);
-  if (enc_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "encode error code: %d\n", enc_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_out_len = vtenc_list_decoded_size_u32(enc_out, enc_out_len);
-  dec_out = (uint32_t *) malloc(dec_out_len * sizeof(uint32_t));
-  if (dec_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_ret_code = vtenc_list_decode_u32(enc_out, enc_out_len, dec_out, dec_out_len);
-  if (dec_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "decode error code: %d\n", dec_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  if ((dec_out_len != list_len) || (memcmp(list, dec_out, list_len * sizeof(uint32_t)) != 0)) {
-    fprintf(stderr, "decoded output different from original input\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-free_and_exit:
-  if (enc_out != NULL) free(enc_out);
-  if (dec_out != NULL) free(dec_out);
-
-  return ret_code;
-}
-
-static int test_encdec_list64(uint64_t *list, size_t list_len)
-{
-  size_t enc_out_cap, enc_out_len, dec_out_len;
-  uint8_t *enc_out = NULL;
-  uint64_t *dec_out = NULL;
-  VtencErrorCode enc_ret_code, dec_ret_code;
-  int ret_code = 1;
-
-  enc_out_cap = vtenc_list_max_encoded_size_u64(list_len);
-  enc_out = (uint8_t *) malloc(enc_out_cap * sizeof(uint8_t));
-  if (enc_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  enc_ret_code = vtenc_list_encode_u64(list, list_len, enc_out, enc_out_cap, &enc_out_len);
-  if (enc_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "encode error code: %d\n", enc_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_out_len = vtenc_list_decoded_size_u64(enc_out, enc_out_len);
-  dec_out = (uint64_t *) malloc(dec_out_len * sizeof(uint64_t));
-  if (dec_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_ret_code = vtenc_list_decode_u64(enc_out, enc_out_len, dec_out, dec_out_len);
-  if (dec_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "decode error code: %d\n", dec_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  if ((dec_out_len != list_len) || (memcmp(list, dec_out, list_len * sizeof(uint64_t)) != 0)) {
-    fprintf(stderr, "decoded output different from original input\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-free_and_exit:
-  if (enc_out != NULL) free(enc_out);
-  if (dec_out != NULL) free(dec_out);
-
-  return ret_code;
-}
-
-static int test_encdec_set8(uint8_t *set, size_t set_len)
-{
-  size_t enc_out_cap, enc_out_len, dec_out_len;
-  uint8_t *enc_out=NULL, *dec_out=NULL;
-  VtencErrorCode enc_ret_code, dec_ret_code;
-  int ret_code = 1;
-
-  enc_out_cap = vtenc_set_max_encoded_size_u8(set_len);
-  enc_out = (uint8_t *) malloc(enc_out_cap * sizeof(uint8_t));
-  if (enc_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  enc_ret_code = vtenc_set_encode_u8(set, set_len, enc_out, enc_out_cap, &enc_out_len);
-  if (enc_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "encode error code: %d\n", enc_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_out_len = vtenc_set_decoded_size_u8(enc_out, enc_out_len);
-  dec_out = (uint8_t *) malloc(dec_out_len * sizeof(uint8_t));
-  if (dec_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_ret_code = vtenc_set_decode_u8(enc_out, enc_out_len, dec_out, dec_out_len);
-  if (dec_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "decode error code: %d\n", dec_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  if ((dec_out_len != set_len) || (memcmp(set, dec_out, set_len) != 0)) {
-    fprintf(stderr, "decoded output different from original input\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-free_and_exit:
-  if (enc_out != NULL) free(enc_out);
-  if (dec_out != NULL) free(dec_out);
-
-  return ret_code;
-}
-
-static int test_encdec_set16(uint16_t *set, size_t set_len)
-{
-  size_t enc_out_cap, enc_out_len, dec_out_len;
-  uint8_t *enc_out = NULL;
-  uint16_t *dec_out = NULL;
-  VtencErrorCode enc_ret_code, dec_ret_code;
-  int ret_code = 1;
-
-  enc_out_cap = vtenc_set_max_encoded_size_u16(set_len);
-  enc_out = (uint8_t *) malloc(enc_out_cap * sizeof(uint8_t));
-  if (enc_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  enc_ret_code = vtenc_set_encode_u16(set, set_len, enc_out, enc_out_cap, &enc_out_len);
-  if (enc_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "encode error code: %d\n", enc_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_out_len = vtenc_set_decoded_size_u16(enc_out, enc_out_len);
-  dec_out = (uint16_t *) malloc(dec_out_len * sizeof(uint16_t));
-  if (dec_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_ret_code = vtenc_set_decode_u16(enc_out, enc_out_len, dec_out, dec_out_len);
-  if (dec_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "decode error code: %d\n", dec_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  if ((dec_out_len != set_len) || (memcmp(set, dec_out, set_len * sizeof(uint16_t)) != 0)) {
-    fprintf(stderr, "decoded output different from original input\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-free_and_exit:
-  if (enc_out != NULL) free(enc_out);
-  if (dec_out != NULL) free(dec_out);
-
-  return ret_code;
-}
-
-static int test_encdec_set32(uint32_t *set, size_t set_len)
-{
-  size_t enc_out_cap, enc_out_len, dec_out_len;
-  uint8_t *enc_out = NULL;
-  uint32_t *dec_out = NULL;
-  VtencErrorCode enc_ret_code, dec_ret_code;
-  int ret_code = 1;
-
-  enc_out_cap = vtenc_set_max_encoded_size_u32(set_len);
-  enc_out = (uint8_t *) malloc(enc_out_cap * sizeof(uint8_t));
-  if (enc_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  enc_ret_code = vtenc_set_encode_u32(set, set_len, enc_out, enc_out_cap, &enc_out_len);
-  if (enc_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "encode error code: %d\n", enc_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_out_len = vtenc_set_decoded_size_u32(enc_out, enc_out_len);
-  dec_out = (uint32_t *) malloc(dec_out_len * sizeof(uint32_t));
-  if (dec_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_ret_code = vtenc_set_decode_u32(enc_out, enc_out_len, dec_out, dec_out_len);
-  if (dec_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "decode error code: %d\n", dec_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  if ((dec_out_len != set_len) || (memcmp(set, dec_out, set_len * sizeof(uint32_t)) != 0)) {
-    fprintf(stderr, "decoded output different from original input\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-free_and_exit:
-  if (enc_out != NULL) free(enc_out);
-  if (dec_out != NULL) free(dec_out);
-
-  return ret_code;
-}
-
-static int test_encdec_set64(uint64_t *set, size_t set_len)
-{
-  size_t enc_out_cap, enc_out_len, dec_out_len;
-  uint8_t *enc_out = NULL;
-  uint64_t *dec_out = NULL;
-  VtencErrorCode enc_ret_code, dec_ret_code;
-  int ret_code = 1;
-
-  enc_out_cap = vtenc_set_max_encoded_size_u64(set_len);
-  enc_out = (uint8_t *) malloc(enc_out_cap * sizeof(uint8_t));
-  if (enc_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  enc_ret_code = vtenc_set_encode_u64(set, set_len, enc_out, enc_out_cap, &enc_out_len);
-  if (enc_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "encode error code: %d\n", enc_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_out_len = vtenc_set_decoded_size_u64(enc_out, enc_out_len);
-  dec_out = (uint64_t *) malloc(dec_out_len * sizeof(uint64_t));
-  if (dec_out == NULL) {
-    fprintf(stderr, "allocation error\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  dec_ret_code = vtenc_set_decode_u64(enc_out, enc_out_len, dec_out, dec_out_len);
-  if (dec_ret_code != VtencErrorNoError) {
-    fprintf(stderr, "decode error code: %d\n", dec_ret_code);
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-  if ((dec_out_len != set_len) || (memcmp(set, dec_out, set_len * sizeof(uint64_t)) != 0)) {
-    fprintf(stderr, "decoded output different from original input\n");
-    ret_code = 0;
-    goto free_and_exit;
-  }
-
-free_and_exit:
-  if (enc_out != NULL) free(enc_out);
-  if (dec_out != NULL) free(dec_out);
-
-  return ret_code;
-}
-
-static int test_list8(FILE *f, size_t seqsize)
-{
-  uint8_t *list = (uint8_t *) malloc(seqsize * sizeof(uint8_t));
-
-  if (!read_arr8(f, list, seqsize)) {
+  if (!read_arr8(f, seq, seqsize)) {
     fprintf(stderr, "Reading sequence failed\n");
-    free(list);
+    free(seq);
     return 0;
   }
 
-  if (!test_encdec_list8(list, seqsize)) {
+  if (!test_encode_and_decode8(encoder, seq, seqsize)) {
     fprintf(stderr, "Encoding/decoding test failed\n");
-    free(list);
+    free(seq);
     return 0;
   }
 
-  free(list);
+  free(seq);
 
   return 1;
 }
 
-static int test_list16(FILE *f, size_t seqsize)
+static int test_seq16(FILE *f, VtencEncoder *encoder, size_t seqsize)
 {
-  uint16_t *list = (uint16_t *) malloc(seqsize * sizeof(uint16_t));
+  uint16_t *seq = (uint16_t *) malloc(seqsize * sizeof(uint16_t));
 
-  if (!read_arr16(f, list, seqsize)) {
+  if (!read_arr16(f, seq, seqsize)) {
     fprintf(stderr, "Reading sequence failed\n");
-    free(list);
+    free(seq);
     return 0;
   }
 
-  if (!test_encdec_list16(list, seqsize)) {
+  if (!test_encode_and_decode16(encoder, seq, seqsize)) {
     fprintf(stderr, "Encoding/decoding test failed\n");
-    free(list);
+    free(seq);
     return 0;
   }
 
-  free(list);
+  free(seq);
 
   return 1;
 }
 
-static int test_list32(FILE *f, size_t seqsize)
+static int test_seq32(FILE *f, VtencEncoder *encoder, size_t seqsize)
 {
-  uint32_t *list = (uint32_t *) malloc(seqsize * sizeof(uint32_t));
+  uint32_t *seq = (uint32_t *) malloc(seqsize * sizeof(uint32_t));
 
-  if (!read_arr32(f, list, seqsize)) {
+  if (!read_arr32(f, seq, seqsize)) {
     fprintf(stderr, "Reading sequence failed\n");
-    free(list);
+    free(seq);
     return 0;
   }
 
-  if (!test_encdec_list32(list, seqsize)) {
+  if (!test_encode_and_decode32(encoder, seq, seqsize)) {
     fprintf(stderr, "Encoding/decoding test failed\n");
-    free(list);
+    free(seq);
     return 0;
   }
 
-  free(list);
+  free(seq);
 
   return 1;
 }
 
-static int test_list64(FILE *f, size_t seqsize)
+static int test_seq64(FILE *f, VtencEncoder *encoder, size_t seqsize)
 {
-  uint64_t *list = (uint64_t *) malloc(seqsize * sizeof(uint64_t));
+  uint64_t *seq = (uint64_t *) malloc(seqsize * sizeof(uint64_t));
 
-  if (!read_arr64(f, list, seqsize)) {
+  if (!read_arr64(f, seq, seqsize)) {
     fprintf(stderr, "Reading sequence failed\n");
-    free(list);
+    free(seq);
     return 0;
   }
 
-  if (!test_encdec_list64(list, seqsize)) {
+  if (!test_encode_and_decode64(encoder, seq, seqsize)) {
     fprintf(stderr, "Encoding/decoding test failed\n");
-    free(list);
+    free(seq);
     return 0;
   }
 
-  free(list);
-
-  return 1;
-}
-
-static int test_set8(FILE *f, size_t seqsize)
-{
-  uint8_t *set = (uint8_t *) malloc(seqsize * sizeof(uint8_t));
-
-  if (!read_arr8(f, set, seqsize)) {
-    fprintf(stderr, "Reading sequence failed\n");
-    free(set);
-    return 0;
-  }
-
-  if (!test_encdec_set8(set, seqsize)) {
-    fprintf(stderr, "Encoding/decoding test failed\n");
-    free(set);
-    return 0;
-  }
-
-  free(set);
-
-  return 1;
-}
-
-static int test_set16(FILE *f, size_t seqsize)
-{
-  uint16_t *set = (uint16_t *) malloc(seqsize * sizeof(uint16_t));
-
-  if (!read_arr16(f, set, seqsize)) {
-    fprintf(stderr, "Reading sequence failed\n");
-    free(set);
-    return 0;
-  }
-
-  if (!test_encdec_set16(set, seqsize)) {
-    fprintf(stderr, "Encoding/decoding test failed\n");
-    free(set);
-    return 0;
-  }
-
-  free(set);
-
-  return 1;
-}
-
-static int test_set32(FILE *f, size_t seqsize)
-{
-  uint32_t *set = (uint32_t *) malloc(seqsize * sizeof(uint32_t));
-
-  if (!read_arr32(f, set, seqsize)) {
-    fprintf(stderr, "Reading sequence failed\n");
-    free(set);
-    return 0;
-  }
-
-  if (!test_encdec_set32(set, seqsize)) {
-    fprintf(stderr, "Encoding/decoding test failed\n");
-    free(set);
-    return 0;
-  }
-
-  free(set);
-
-  return 1;
-}
-
-static int test_set64(FILE *f, size_t seqsize)
-{
-  uint64_t *set = (uint64_t *) malloc(seqsize * sizeof(uint64_t));
-
-  if (!read_arr64(f, set, seqsize)) {
-    fprintf(stderr, "Reading sequence failed\n");
-    free(set);
-    return 0;
-  }
-
-  if (!test_encdec_set64(set, seqsize)) {
-    fprintf(stderr, "Encoding/decoding test failed\n");
-    free(set);
-    return 0;
-  }
-
-  free(set);
+  free(seq);
 
   return 1;
 }
 
 static int test_sequence(FILE *f, const struct SequenceAttr *attr)
 {
-  if (attr->islist) {
-    switch (attr->width) {
-      case 8: return test_list8(f, attr->size);
-      case 16: return test_list16(f, attr->size);
-      case 32: return test_list32(f, attr->size);
-      case 64: return test_list64(f, attr->size);
-      default: return 0;
-    }
-  } else {
-    switch (attr->width) {
-      case 8: return test_set8(f, attr->size);
-      case 16: return test_set16(f, attr->size);
-      case 32: return test_set32(f, attr->size);
-      case 64: return test_set64(f, attr->size);
-      default: return 0;
-    }
+  VtencEncoder encoder;
+
+  vtenc_encoder_init(&encoder);
+
+  if (!attr->islist) {
+    encoder.has_repeated_values = 0;
+    encoder.skip_full_subtrees = 1;
+  }
+
+  switch (attr->width) {
+    case 8: return test_seq8(f, &encoder, attr->size);
+    case 16: return test_seq16(f, &encoder, attr->size);
+    case 32: return test_seq32(f, &encoder, attr->size);
+    case 64: return test_seq64(f, &encoder, attr->size);
+    default: return 0;
   }
 
   return 0;
